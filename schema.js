a12e978f190631fee9686a4352a5bfc2d7551378
Dave Barthly <davebarthly@gmail.com>
2017-09-19 03:56:35 -0400

    Build out graphql schema

--- schema.js
+++ schema.js
@@ -3,22 +3,25 @@ const axios = require("axios");
 const { 
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLString
 } = require('graphql')
 
-const url = "https://ghibliapi.herokuapp.com";
+// const url = "https://ghibliapi.herokuapp.com";
 
-const x = axios(url)
-.then(res => console.log(res))
-.catch(err =>  {
-  console.log("bad")
-  console.log(err)
-});
+// const x = axios(url)
+// .then(res => console.log(res))
+// .catch(err =>  {
+//   console.log("bad")
+//   console.log(err)
+// });
 
 const FlimType = new GraphQLObjectType ({
-  title: 'Flim',
+  name: 'Flim',
   description: '...',
   fields: () => ({
-
+    title: {
+      type: GraphQLString
+    }
   })
 })
 
@@ -26,12 +29,22 @@ module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     description: '...',
-    field: () => ({
+    fields: () => ({
       flims: {
         type: FlimType,
         args: {
           id: { type: GraphQLString }
-        }
+        },
+        resolve: (root, args) => axios("https://ghibliapi.herokuapp.com", {
+          parems: {
+            
+          }
+        })
+        .then(res => console.log(res))
+        .catch(err =>  {
+          console.log("bad")
+          console.log(err)
+        })
       }
     })
   })
