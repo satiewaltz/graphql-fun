857060eee64b932bbb726449db2a59b23cdf65f7
Dave Barthly <davebarthly@gmail.com>
2017-09-20 02:24:09 -0400

    Add species queries and collection fields

--- schema.js
+++ schema.js
@@ -8,39 +8,78 @@ const {
   GraphQLList
 } = require('graphql')
 
-const url = "https://ghibliapi.herokuapp.com/films/";
+const url = "https://ghibliapi.herokuapp.com/";
 
 const FilmType = new GraphQLObjectType ({
-  name: 'Flim',
+  name: 'Film',
   description: '...',
   fields: () => ({
     title: {
       type: GraphQLString,
       resolve: json => json.title
-    }
+    },
+    description: {
+      type: GraphQLString,
+      resolve: json => json.description
+    }    
+  })
+})
+
+const SpeciesType = new GraphQLObjectType ({
+  name: 'Species',
+  description: '...',
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve: json => json.name
+    },
+    classification: {
+      type: GraphQLString,
+      resolve: json => json.classification
+    }    
   })
 })
 
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'Query',
+    name: 'query',
     description: '...',
     fields: () => ({
-      flims: {
+      // Entire resource
+      films: {
         type: new GraphQLList(FilmType),
+        resolve: (root, args) => axios(url + `films`)
+          .then(res => res.data)
+          .catch(err => err)
+      },
+      // Singleton
+      film: {
+        type: FilmType,
         args: {
           id: { type: GraphQLInt }
         },
-        resolve: (root, args) => axios(url)
-          .then(res => {            
-            if (typeof args.id === "undefined") {
-              return res.data;
-            } else {
-              return res.data[args.id];
-            }
-          })
+        resolve: (root, args) => axios(url + `films`)
+          .then(res => res.data[args.id])
           .catch(err => err)
-      }
+      },
+      
+      // Entire resource
+      species: {
+        type: new GraphQLList(SpeciesType),
+        resolve: (root, args) => axios(url + `species`)
+          .then(res => res.data)
+          .catch(err => err)
+      },
+      // Singleton
+      singleSpecies: {
+        type: SpeciesType,
+        args: {
+          id: { type: GraphQLInt }
+        },
+        resolve: (root, args) => axios(url + `species`)
+          .then(res => res.data[args.id])
+          .catch(err => err)
+      },      
     })
   })
 })
